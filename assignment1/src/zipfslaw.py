9b29b6cd15575cba2b438a152c1b311dc4f49ee4
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2014-11-02 18:54:04 +0100

    fine tuning zipfs law, utf8 output, aspect ratio for log log plot

--- assignment1/src/zipfslaw.py
+++ assignment1/src/zipfslaw.py
@@ -6,13 +6,17 @@ from collections import Counter
 import matplotlib
 import matplotlib.pyplot as plt
 import io
+import codecs
 
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
 
 def read_and_tokenize(filename):
   with io.open(filename, "rt", encoding="utf-8") as f:
     text = f.read()
     tokenized = nltk.word_tokenize(text)
     tokenized = filter(lambda word: word.isalpha(), tokenized)
+    tokenized = map(lambda word: word.lower(), tokenized)
     return tokenized
 
 if __name__ == "__main__":
@@ -20,6 +24,7 @@ if __name__ == "__main__":
   os.chdir(script_dir + "/../assets")
   #do for all
   for filename in filter(lambda filename: not filename.endswith(".png"), os.listdir(".")):
+    print "processing %s" % filename
     tokens = read_and_tokenize(filename)
     counts = Counter(tokens)
     counts_only = counts.values()
@@ -29,10 +34,21 @@ if __name__ == "__main__":
     #taking only the first 100 words for the linear plot
     figure.add_subplot(121).plot(counts_only[0:100])
     plt.title("%s linear" % filename)
-    figure.add_subplot(122).loglog(counts_only)
+    figure.add_subplot(122, aspect="equal").loglog(counts_only)
     plt.title("%s loglog" % filename)
     plt.savefig(filename+".png")
-
+    
+    c = []
+    i = 1
+    for word,count in counts.most_common(len(counts)):
+      frequency_rank = float(count)/len(tokens) * i
+      c.append(frequency_rank)
+      i+=1
+      if i < 20:
+        print "%s:%d -> %.3f" % (word, count, frequency_rank)
+    average_c = sum(c)/len(c)
+    print "average c: %.3f" % average_c
+    print ""
 
 
 
